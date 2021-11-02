38ae80a02504b946c7f1f506eee88607347d3377
Prince Jan Kumi <janprince002@gmail.com>
2021-11-02 21:35:19 +0000

    modified environment variable

--- bot_app/urls.py
+++ bot_app/urls.py
@@ -5,5 +5,5 @@ import os
 app_name = "bot_app"
 urlpatterns= [
     # path("", views.index, name="index"),
-    path(f"{os.environ['TOKEN']/}", views.index, name="index"),
+    path(f"{os.environ['TOKEN']}", views.index, name="index"),
 ]

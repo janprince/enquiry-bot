4a399c2c7d9d3387f124217d356bdc5e3094ca40
Prince Jan Kumi <janprince002@gmail.com>
2021-11-13 20:45:13 +0000

    updated bot_logic with a few docstrings

--- bot_app/bot_logic.py
+++ bot_app/bot_logic.py
@@ -14,7 +14,43 @@ token = os.environ["TOKEN"]
 url = f"https://api.telegram.org/bot{token}"
 
 
-#  functions
+"""-------------------Conditional statements----------------------"""
+
+def generate_response(firstname, chat_id, msg):
+    """
+    :param firstname: effective_user_name
+    :param chat_id: effective_chat_id
+    :param msg: message
+
+    checks for patterns in message and calls the appropriate response function.
+    """
+    msg = msg.lower()
+
+    if "hi" in msg or "hello" in msg or "good" in msg:
+        greet(firstname, chat_id)
+    elif "/start" in msg:
+        index(firstname, chat_id)
+    elif "/about" in msg or "/info" in msg:
+        info(chat_id)
+    elif "/enquiry" in msg or "enquiry" in msg:
+        enquiry(chat_id)
+    elif "/compliant" in msg or "complaint" in msg:
+        complaint(chat_id)
+    elif "/cancel" in msg or "no, that's it for now" in msg or '/exit' in msg or 'bye' in msg:
+        exit(firstname, chat_id)
+    elif "fees and dues" in msg:
+        fees(chat_id)
+    elif "hall fees" in msg:
+        fee(chat_id, 'hall')
+    elif "jcr fees" in msg:
+        fee(chat_id, 'jcr')
+    else:
+        exception(chat_id)
+
+
+
+"""----------------------------------------------- General --------------------------------------------------------"""
+
 
 def index(firstname, chat_id):
     response_text = f"""
@@ -35,21 +71,64 @@ def index(firstname, chat_id):
 
 def info(chat_id):
     about_text = """
-        Jubilee Hall, located on the southern part of the campus, adjacent to the
-        International Students‘ Hostel, was built to commemorate the University‘s Golden Jubilee
-        celebration in 1998. Modeled after Akuafo Hall, one of the traditional Halls of the University,
-        and funded mainly by alumni of the University, the Hall is a group of 4 (four) multi-purpose
-        blocks containing single study bedrooms, self-contained flats and double rooms. Facilities in
-        the Hall include common rooms, libraries and restaurants. There are rooms suitable for
-        disabled students.
+        Jubilee Hall, located on the southern part of the campus, adjacent to the International Students‘ Hostel, 
+        was built to commemorate the University‘s Golden Jubilee celebration in 1998. Modeled after Akuafo Hall, 
+        one of the traditional Halls of the University, and funded mainly by alumni of the University, 
+        the Hall is a group of 4 (four) multi-purpose blocks containing single study bedrooms, self-contained 
+        flats and double rooms. Facilities in the Hall include common rooms, libraries and restaurants. 
+        There are rooms suitable for disabled students.
         \n
-        \n GPS Address:
-        \n Contact:
+        \n GPS Address: John Doe
+        \n Contact:     John Doe
         \n
-        \n <b>JCR President</b>:
-        \n Hall Head / Senior Tutor:
+        \n <b>JCR President</b>:    John Doe
+        \n <b>Hall Head / Senior Tutor</b>: John Doe
     """
-    r = requests.get(f"{url}/sendMessage", params={"chat_id": chat_id, "text": about_text})
+    r = requests.get(f"{url}/sendMessage", params={"chat_id": chat_id, "text": about_text, "parse_mode":"HTML"})
+
+
+def greet(name, chat_id):
+    response = f"Hello, {name}"
+    r = requests.get(f"{url}/sendMessage", params={"chat_id": chat_id, "text": response})
+
+
+def menu(chat_id):
+    response_text = f"""
+        <b>Commands</b>
+        \n Use /start to start a conversation
+        \n Use /menu to get the list of commands.
+        \n Use /enquiry if you want to make an enquiry.
+        \n Use /complaint to lodge a complaint
+        \n Use /cancel or /exit to cancel a conversation
+        \n\n Use /about or /info to read about Jubilee Hall.
+    """
+
+    # ReplyKeyboardMarkup Object
+    keyboard_markup = {"keyboard": [["Make an Enquiry", "Lodge a complaint"]], "one_time_keyboard": True}
+    reply_keyboard_markup = json.dumps(keyboard_markup)    # converts dict to json
+
+    r = requests.get(f"{url}/sendMessage", params={"chat_id": chat_id, "text": response_text, "reply_markup": reply_keyboard_markup, "parse_mode":"HTML"})
+
+
+def exception(chat_id):
+    response = "Sorry, I don't quite get that."
+    r = requests.get(f"{url}/sendMessage", params={"chat_id": chat_id, "text": response})
+
+
+def exit(name, chat_id):
+    response = "👋 Bye {} I hope we can talk again some day.".format(name)
+    r = requests.get(f"{url}/sendMessage", params={"chat_id": chat_id, "text": response, "remove_keyboard": True})
+
+
+# def send_msg(chat_id, response_text):
+#     token = os.environ["TOKEN"]
+#     url = f"https://api.telegram.org/bot{token}"
+#     reply_markup={"keyboard":[["Make an Enquiry","Lodge a complaint"]],"one_time_keyboard":True}
+#     x = json.dumps(reply_markup)
+#     r = requests.get(f"{url}/sendMessage", params={"chat_id": chat_id, "text": response_text, "reply_markup": x})
+
+
+"""----------------------------------------------- Enquiry --------------------------------------------------------"""
 
 
 def enquiry(chat_id):
@@ -60,62 +139,6 @@ def enquiry(chat_id):
     reply_keyboard_markup = json.dumps(keyboard_markup)
     r = requests.get(f"{url}/sendMessage", params={"chat_id": chat_id, "text": response, "reply_markup": reply_keyboard_markup})
 
-
-def complaint(chat_id):
-    response = "What is your enquiry about?"
-
-    # ReplyKeyboardMarkup Object
-    keyboard_markup = {"keyboard": [["qwerty", "qwerty"]], "one_time_keyboard": True}
-    reply_keyboard_markup = json.dumps(keyboard_markup)
-    r = requests.get(f"{url}/sendMessage", params={"chat_id": chat_id, "text": response, "reply_markup": reply_keyboard_markup})
-
-
-def greet(name, chat_id):
-    response = f"Hello, {name}"
-    r = requests.get(f"{url}/sendMessage", params={"chat_id": chat_id, "text": response})
-
-
-def exception(chat_id):
-    response = "Sorry, I don't quite get that."
-    r = requests.get(f"{url}/sendMessage", params={"chat_id": chat_id, "text": response})
-
-
-def exit(name, chat_id):
-    response = "👋 Bye {} I hope we can talk again some day.".format(name)
-    r = requests.get(f"{url}/sendMessage", params={"chat_id": chat_id, "text": response})
-
-
-def generate_response(firstname, chat_id, msg):
-    msg = msg.lower()
-
-    if "hi" in msg or "hello" in msg or "good" in msg:
-        greet(firstname, chat_id)
-    elif "/start" in msg:
-        index(firstname, chat_id)
-    elif "/about" in msg or "/info" in msg:
-        info(chat_id)
-    elif "/enquiry" in msg or "enquiry" in msg:
-        enquiry(chat_id)
-    elif "/compliant" in msg or "complaint" in msg:
-        complaint(chat_id)
-    elif "/cancel" in msg or "no, that's it for now" in msg:
-        exit(firstname, chat_id)
-    elif "fees and dues" in msg:
-        fees(chat_id)
-    elif "hall fees" in msg:
-        fee(chat_id, 'hall')
-    elif "jcr fees" in msg:
-        fee(chat_id, 'jcr')
-    else:
-        exception(chat_id)
-
-
-# def send_msg(chat_id, response_text):
-#     token = os.environ["TOKEN"]
-#     url = f"https://api.telegram.org/bot{token}"
-#     reply_markup={"keyboard":[["Make an Enquiry","Lodge a complaint"]],"one_time_keyboard":True}
-#     x = json.dumps(reply_markup)
-#     r = requests.get(f"{url}/sendMessage", params={"chat_id": chat_id, "text": response_text, "reply_markup": x})
 
 # fees
 def fees(chat_id):
@@ -149,6 +172,27 @@ def fee(chat_id, type):
     # ask user if there is another enquiry.
     another_enquiry(chat_id)
 
+
+# hall accomodation
+
+# jcr
+
+
+
+"""----------------------------------------------- Complaint --------------------------------------------------------"""
+
+
+def complaint(chat_id):
+    response = "What is your enquiry about?"
+
+    # ReplyKeyboardMarkup Object
+    keyboard_markup = {"keyboard": [["qwerty", "qwerty"]], "one_time_keyboard": True}
+    reply_keyboard_markup = json.dumps(keyboard_markup)
+    r = requests.get(f"{url}/sendMessage", params={"chat_id": chat_id, "text": response, "reply_markup": reply_keyboard_markup})
+
+
+
+"""---------------------------------------- User done ------------------------------------------------------"""
 
 def another_enquiry(chat_id):
     """
--- bot_app/models.py
+++ bot_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class Response(models.Model):
+    response_text = models.TextField(blank=True)
+    keyboard_options = models.CharField(max_length=500)  # this
+
+
+class Command(models.Model):
+    command = models.CharField(max_length=255, blank=True)
+    response = models.ForeignKey(Response, null=True, on_delete=models.SET_NULL, related_name="comma")
